3ae793276ef2ed4fee7b7e3cdc2cafa99aadc728
jomlli709 <jgambrell@una.edu>
2024-11-06 13:08:59 -0600

    Update read-data.js

--- Scripts/read-data.js
+++ Scripts/read-data.js
@@ -1,30 +1,14 @@
+document.getElementById('checkWeather').addEventListener('click', getWeather);
 
-
-// $.ajax({
-//   dataType: "json",
-//   url: url,
-//   data: data,
-//   success: success
-// });
-
-
-$("button").click(function(){
-    $.getJSON("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m", function(result){
-      $.each(result, function(i, field){
-        $("div").append(field + " ");
-      });
-    });
-  });
-
-// $.getJSON( "https://barrycumbie.github.io/barrycumbie/data.js", function( data ) {
-//     console.log(data); 
-//     var items = [];
-//     $.each( data, function( key, val ) {
-//       items.push( "<li id='" + key + "'>" + val + "</li>" );
-//     });
-   
-//     $( "<ul/>", {
-//       "class": "my-new-list",
-//       html: items.join( "" )
-//     }).appendTo( "body" );
-//   });
+function getWeather() {
+  fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true")
+    .then(response => response.json())
+    .then(data => {
+      const current = data.current_weather;
+      document.getElementById('weatherInfo').innerHTML = `
+        <p>Temperature: ${current.temperature}°C</p>
+        <p>Wind Speed: ${current.windspeed} m/s</p>
+      `;
+    })
+    .catch(error => console.error("Error fetching data:", error));
+}
